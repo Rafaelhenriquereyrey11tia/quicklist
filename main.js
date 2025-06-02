027256155873656526ed6dd498d5b2f880e43e7c
Rafael Henrique Rey Rey <rhreyrey@gmail.com>
2025-06-02 02:49:56 -0300

    problema que não estava marcando a lista , ja resolvido

--- main.js
+++ main.js
@@ -15,47 +15,31 @@ function addItem(){
     showItemsList()
 }
 
-function showItemsList(){
+function showItemsList() {
+    const sectionList = document.querySelector(".list");
+    sectionList.innerHTML = "";
 
-    const sectionList = document.querySelector(".list")
-    sectionList.textContent =""
-
-    items.map((item,index)=>{
-
-    sectionList.innerHTML += ` <div class="item">
-            <div>
-                    <input type="checkbox" name="list" id="item-${index}">
-                <div class="custom-checkbox" onclick="checkItem('${item.name}')">
-                    <img src="./assets/checked.svg" alt="checked">
+    items.forEach((item, index) => {
+        sectionList.innerHTML += `
+            <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked ? "checked" : ""} onclick="checkItem('${item.name}')">
+                    <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+                    <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
                 </div>
-                <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+                <button onclick="removeItem('${item.name}')">
+                    <img src="./assets/trash-icon.svg" alt="trash icon">
+                </button>
             </div>
-            <button onclick="removeItem('${item.name}')">
-                <img src="./assets/trash-icon.svg" alt="trash icon">
-            </button>
-        </div>`
-
-    })
-}
-function removeItem(itemName){
- const itemIndex = items.findIndex((item)=> item.name === itemName)
- const divWarning = document.querySelector(".warning")
-
- divWarning.classList.remove("hide-warning")
-
- setTimeout(()=>{
-    divWarning.classList.add("hide-warning")
-},4000)
-
- if(itemIndex!==-1){
-    items.splice(itemIndex,1)
- }
- showItemsList();
-
+        `;
+    });
 }
 
-function checkItem(itemName){
- const item = items.find((item)=> item.name === itemName)
-item.checked = !item.checked
- showItemsList()
-}
+function checkItem(itemName) {
+    const item = items.find(item => item.name === itemName);
+    if (item) {
+        item.checked = !item.checked;
+    }
+    showItemsList();}
