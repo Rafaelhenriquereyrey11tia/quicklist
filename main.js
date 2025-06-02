7cd7c55f28e8796dc2a8055ad7cd0845ff25f763
Rafael Henrique Rey Rey <rhreyrey@gmail.com>
2025-06-02 01:20:28 -0300

    continuação da função de criar novos itens criaando o objet o de cacda item para ser adicionado ao array de items

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ const items=[]
 function addItem(){
     const itemName = document.querySelector("#item").value
 
-    
+    const item={
+        name:itemName,
+        checked:false
+    }
+    items.push(item)
+
 }
